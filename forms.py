b934d5b22cf855a5d0d20aff9fd358e7c0b817a8
stephen osemeke <osemekestephen1995@outlook.com>
2021-08-20 00:42:34 +0200

    forms commented

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
-from django.forms import ModelForm
+""" from django.forms import ModelForm
 from models import Contact
 
 class ContactForm(Model.Form):
 	class Meta:
 		model = Contact
-		fields = '__all__'
+		fields = '__all__' """
