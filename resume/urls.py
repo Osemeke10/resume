a8d5f20d25d41bfc8142cac89421d7b00ad8ba40
stephen osemeke <osemekestephen1995@outlook.com>
2021-08-19 19:56:05 +0200

    second comit

--- resume/urls.py
+++ resume/urls.py
@@ -1,8 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.config.urls.static import static
+from django.config import settings
+
+from django.views.static import serve
+from django.config.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('resumesite.urls')),
+    path('/submit', )
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
